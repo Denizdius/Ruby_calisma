8648b1a9515d2858cacfe1e52e4ef49ae1136f38
Denizdius <47947320+Denizdius@users.noreply.github.com>
2019-04-19 14:19:54 +0300

    Add files via upload

--- deneme2.rb
+++ deneme2.rb
@@ -1,17 +1,27 @@
-
 puts "Yeni bir şey yapacağız önce sen birkaç sayı gireceksen sonra o sayılarlara bir döngü üreteceğeiz"
 sayıbir = gets.to_i
 sayıiki = gets.to_i
 sayıüç = gets.to_i
 sayıdört = gets.to_i
 sayıbeş = gets.to_i
-x = [sayıbir,sayıiki,sayıüç,sayıdört,sayıbeş]
+x = [sayibir,sayiiki,sayiüç,sayidört,sayibeş]
 puts "İşlem seç lo (+,/,*,-)"
 sonuc = 0
 islem = gets.chomp
 while x >= 1 
     if islem == "*"
-        sonuc = dizi * sayıbir
+        sonuc = x * sayibir
+    elsif islem == "*"
+        sonuc = x * sayibir
+    elsif islem == "/"
+        sonuc = x.to_f / sayibir    
+    elsif islem == "-"
+        sonuc = (x-sayibir).abs
+    elsif islem == "+"
+        sonuc = x + sayibir
+    end
+
+                
             
     
             
