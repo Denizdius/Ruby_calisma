3fd03d9569123eb0ef6cfa307853db93f573c1a1
Denizdius <26deniz53@gmail.com>
2019-08-28 16:01:42 +0300

    finish

--- primefinder.rb
+++ primefinder.rb
@@ -1,26 +1,29 @@
 # require 'prime'
 # print Prime.first(10_001).to_a.last
 # #istediğime ulaştım sonucusunu almak için yazılcsk şeyi unutum
+def primest(n)
+    primes_counter = 0
+    number = 1
 
-primes_counter = 0
-number = 1
+    while (true)
+        number += 1
+        prime = true
+        2.upto(Math.sqrt(number)) do |div|
+            if (number % div == 0)
+                prime = false
+                break
+            end
+        end
 
-while (true)
-    number += 1
-    prime = true
-    2.upto(Math.sqrt(number)) do |div|
-        if (number % div == 0)
-            prime = false
+        if prime
+            primes_counter += 1
+        end
+
+        if primes_counter == n
+             return number
             break
         end
     end
-
-    if prime
-        primes_counter += 1
-    end
-
-    if primes_counter == 10_001
-        puts "10_001. asal sayı #{number}"
-        break
-    end
 end
+
+puts primest(10_001)
