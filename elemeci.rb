eda11f8550f4dd05644279b99a0f010916631cd7
Denizdius <26deniz53@gmail.com>
2019-10-12 17:25:29 +0300

    bum

--- elemeci.rb
+++ elemeci.rb
@@ -56,21 +56,64 @@ while (true)
       print "Sabıka Kaydı[E|H]: "
       criminal_record = gets.chomp.upcase
       candidate[:criminal_record] = ((criminal_record == 'E') ? true : false)
-      
+      exit(0) if candidate[:criminal] = "E" then puts "Üzgünüz şirketimiz temiz insanlarla çalışır çıkış yapılıyor..."
+
       known_language_count = 0
       %w[Ruby Python C Java].each do |lang|
         print "#{lang} dilini biliyor mu?[E|H]"
         lang_answer = gets.chomp.upcase
         known_language_count += (lang_answer == 'E' ? 1 : 0)
+        exit(0) if known_language_count == 0 then puts "Üzgünüz ama bu iş için en az bunların içinden birini bilmelsiniz çıkış yapılıyor..."
       end
       candidate[:known_language_count] = known_language_count
 
       print "Experince of years: "
       candidate[:experience] = gets.to_i
+      exit(0) if candidate[:experience] > candidate[:age] then puts "Tutarsız cevap,çıkış yapılıyor bip,bop,bip"
+      exit(0) if candidate[:experience] < 5 then puts "Üzgünüz şirketimiz daha tecrubeli insanlar arıyor,çıkış yapılıyor..."
 
       candidates << candidate
     when 2 then puts "Aday İstatistikleri"
-    when 3 
+
+      candidate = {}
+
+      print "Bir üniversteden mezun oldunuz mu ? (E/H)"
+      candidate[:universty] = gets
+
+      print "Hangi üniversteden mezun oldunuz"
+      candidate[:wuniversty] = gets
+      if candidate[:universty] == "H" then candidate[:wuniversty] == "Mezun olmadım!"
+      
+      print "Diploma notunuz ? (sayıyla belirtilecek) "
+      candidate[:finalscore] = gets.to_i
+      if candidate[:universty] == "H" then candidate[:finalscore] == "0"
+       
+      print "Önceki işiniz nedir ?"
+      candidate[:previousjob] = gets.to_s
+
+      print "Hobileriniz nedir ?"
+      candidate[:hobbies] = gets.to_s
+
+      print "En sevdiğiniz programlama dili nedir ?"
+      candidate[:favlanguage] = gets.to_s
+
+      print "Hangi Saatlerde çalışmak istersiniz (9.00-18.00) yoksa (18.00-03.00) "
+      candidate[:timeprefrence] = gets.to_s
+
+      print "Çalışmaya başlayınca aklınızdaki yaklaşık maaş tutarı nedir ? (sayıyla belirtilecek) "
+      candidate[:money] = gets.to_i
+
+
+       
+      
+      
+
+
+    
+
+
+
+    when 3 then puts "Eleme"
       pp candidates
     else 
       puts "Programı kullanıdığınız için teşekkürler."
